68c55817fbc7d0cf7fe015c9b272faee540b08bd
yahnodewit-libis <yahno.dewit@kuleuven.be>
2026-07-22 09:32:27 +0200

    fix bug

--- snippets/annotatorComponents/annotatorNavigation.php
+++ snippets/annotatorComponents/annotatorNavigation.php
@@ -1,8 +1,8 @@
 <div class="annotation-pagination <?= $extraClasses ?? '' ?>" data-page="<?=$currentPage?>" data-pages="<?=$countPages?>">
   <div class="prev-arrow prev-icon pagination-icon">
-    <?= svg($kirby->root('plugins').'/libis-annotator/assets/icons/long-arrow.svg') ?>
+    <?= svg($kirby->root('plugins').'/annotator/assets/icons/long-arrow.svg') ?>
   </div>
   <div class="next-arrow next-icon pagination-icon">
-    <?= svg($kirby->root('plugins').'/libis-annotator/assets/icons/long-arrow.svg') ?>
+    <?= svg($kirby->root('plugins').'/annotator/assets/icons/long-arrow.svg') ?>
   </div>
 </div>
