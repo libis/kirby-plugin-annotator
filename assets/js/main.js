8a229760da967bcf2edd2391f3e680884b101068
yahnodewit-libis <yahno.dewit@kuleuven.be>
2026-08-11 14:52:20 +0200

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-import { annotatorBlock } from '../js/annotator.js';
+import { annotatorBlock } from 'annotator.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const annotators = document.querySelectorAll('.annotator-field-section-wrapper');
